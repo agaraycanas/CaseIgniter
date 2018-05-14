f5beda68ed97a99902150f435a39843feeaf073c
Alberto Garay <agaraycanas@educa.madrid.org>
2018-05-14 18:20:37 +0200

    Pequeños ajustes

--- CaseIgniter/application/views/_templates/header.php
+++ CaseIgniter/application/views/_templates/header.php
@@ -8,24 +8,24 @@
 			<?php if (isset ($header['user']) && isset ($header['rol']) ) : ?>
 				<form action="<?=base_url().$header['login_bean'].'/update'?>" method="post">
 				<input type="hidden" name="id" value="<?= $header['user']->id ?>" />
-				<input type="submit" class="btn btn-primary" value="<?= $header['user']->loginname ?>" />
+				<input type="submit" class="btn btn-primary btn-sm" value="<?= $header['user']->loginname ?>" />
 			</form>
 			<form action="<?=base_url().$header['login_bean'].'/changeRol'?>"
 				method="post">
 				<input type="hidden" name="id" value="<?= $header['user']->id ?>" />
-				<input type="submit" class="btn btn-secondary" value="<?= $header['rol']->descripcion ?>" />
+				<input type="submit" class="btn btn-secondary btn-sm" value="<?= $header['rol']->descripcion ?>" />
 			</form>
 			<form action="<?=base_url().$header['login_bean'].'/logout'?>">
-				<input type="submit" class="btn btn-info" value="Cerrar sesión" />
+				<input type="submit" class="btn btn-info btn-sm" value="Cerrar sesión" />
 			</form>
 			<?php else: ?>
 				<?php if (isset ($header['login_bean'])): ?>
 
 					<form  action="<?=base_url().$header['login_bean'].'/login'?>"  >
-						<input type="submit" class="btn btn-primary" value="ENTRAR" />
+						<input type="submit" class="btn btn-primary btn-sm" value="ENTRAR" />
 					</form>
 					<form  action="<?=base_url().$header['login_bean'].'/create'?>"  >
-						<input type="submit" class="btn btn-secondary" value="Crear cuenta" />
+						<input type="submit" class="btn btn-secondary btn-sm" value="Crear cuenta" />
 					</form>
 				<?php endif; ?>
 			<?php endif; ?>
